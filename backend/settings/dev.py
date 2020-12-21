1a03b0d5a2eadd2f33a087adabb116fea16510d3
Exist <bolk9652@naver.com>
2020-12-21 15:31:31 +0900

    📄docs: Initial commit and default setting
    # pip install
    - django-environ
    # edit dir structure
    - settings.py > settings/...
    - requirements > requirements/...

---
+++ backend/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
---
+++ backend/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+
+ALLOWED_HOSTS = [
+    # ...
+]
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    # ...
+}
