69557b4035defdc7358787e7ba4b22a0a5fdebe4
Exist <bolk9652@naver.com>
2020-12-22 15:34:26 +0900

    🐞fix: edit static settings

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -25,5 +25,6 @@ ALLOWED_HOSTS = []
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / "static"
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "_static"]
+MEDIA_ROOT = BASE_DIR / "_media"
