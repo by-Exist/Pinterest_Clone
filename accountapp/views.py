faee62cf00f4f7785be928d3b5549e5ac3ab9951
Exist <bolk9652@naver.com>
2020-12-22 22:35:17 +0900

    🖥️wip: accountapp...
    # accountapp
    - templates, views, urls...
    # backend
    - static setting, url...

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
+from .decorators import account_ownership_required
+from .forms import AccountUpdateForm
 
-# Create your views here.
+
+has_ownership = [account_ownership_required, login_required]
+
+
+class AccountCreateView(CreateView):
+    model = get_user_model()
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accountapp:login")
+    template_name = "accountapp/create.html"
+
+
+class AccountDetailView(DetailView):
+    model = get_user_model()
+    context_object_name = "target_user"
+    template_name = "accountapp/detail.html"
+
+
+@method_decorator(has_ownership, "get")
+@method_decorator(has_ownership, "post")
+class AccountUpdateView(UpdateView):
+    model = get_user_model()
+    context_object_name = "target_user"
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/update.html"
+
+
+@method_decorator(has_ownership, "get")
+@method_decorator(has_ownership, "post")
+class AccountDeleteView(DeleteView):
+    model = get_user_model()
+    context_object_name = "target_user"
+    success_url = reverse_lazy("accountapp:login")
+    template_name = "accountapp/delete.html"
--- backend/settings/base.py
+++ backend/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls.base import reverse_lazy
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third Apps
     # Local Apps
+    "accountapp",
+    "bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -107,3 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
+
+LOGIN_REDIRECT_URL = reverse_lazy("accountapp:home")
