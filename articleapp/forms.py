855cd71e9e2491b153ef3b991df28682bc90e77c
Exist <bolk9652@naver.com>
2020-12-28 17:14:40 +0900

    🐞fix: Article content => WYSIWYG

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,8 +1,15 @@
+from django import forms
 from articleapp.models import Article
 from django.forms import ModelForm
 
 
 class ArticleCreationForm(ModelForm):
+
+    content = forms.Textarea
+
     class Meta:
         model = Article
         fields = ["title", "image", "content", "project"]
+        widgets = {
+            'content': forms.Textarea(attrs={"class": 'editable'}),
+        }
--- articleapp/models.py
+++ articleapp/models.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.forms import widgets
 from projectapp.models import Project
 
 
@@ -12,9 +14,9 @@ class Article(models.Model):
         related_name="article_set",
     )
     project = models.ForeignKey(
-        Project, on_delete=models.SET_NULL, related_name="article_set", null=True
+        Project, on_delete=models.SET_NULL, related_name="article_set", null=True, blank=True
     )
     title = models.CharField("제목", max_length=200, null=True)
     image = models.ImageField("이미지", upload_to="article/%Y/%m/%d", null=False)
-    content = models.TextField("본문", null=True)
+    content = models.TextField("본문", null=True, blank=True)
     created_at = models.DateTimeField("작성 날짜", auto_now_add=True)
