75e253a57d41bc30862af500461ea0e796076149
Exist <bolk9652@naver.com>
2020-12-29 20:18:43 +0900

    🐞fix: testing...

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,11 +1,16 @@
 from .base import *
 
+# TODO: 임시.
 ALLOWED_HOSTS = [
-    # ...
+    "*",
 ]
 
+# TODO: 임시.
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    # ...
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
