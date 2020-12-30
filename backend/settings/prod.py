04e124efa67f537004d644e03a92ce2f8fc1c590
Exist <bolk9652@naver.com>
2020-12-30 16:48:10 +0900

    🖥️wip: prod env setting...

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,9 +1,17 @@
 from .base import *
 
-# TODO: django-envrion을 활용하여 bind(또는, 파일을 secret처럼 다루는 방식이 있나?)를 통해 여러 환경변수(비밀번호, DB 도메인 등등...)를 활용하는 방식이 더 적절하겠지만 우선 강좌대로 진행하자. 또한 Docker 스웜이나 컴포즈를 활용할 줄 알게 된다면 .env 파일을 사용하는 것이 아닌 다른 방식으로 구성하게 될 지도 모른다.
+def read_secret(secret_name):
+    with open(f"/run/secrets/{secret_name}") as f:
+        secret = f.read().strip()
+
+    return secret
+
+# TODO: django-envrion을 활용하여 bind(또는, 파일을 secret처럼 다루는 방식이 있나?)를 통해 여러 환경변수(비밀번호, DB 도메인 등등...)를 활용하는 방식이 더 적절할 지, Docker secret을 활용하는 것이 더 적절할 지 잘 모르겠다.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -14,7 +22,7 @@ DATABASES = {
         # 환경변수들
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret("POSTGRES_PASSWORD"),
         
         # 같은 네트워크로 묶인 컨테이너의 Name.
         'HOST': 'postgres',
